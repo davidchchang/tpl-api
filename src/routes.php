d477212d97c19f4357f2e1bab44aa88af5ddd78c
David Chang <david.chang@chefsplate.com>
2016-04-24 00:27:38 -0400

    Add new /branches endpoint

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,18 @@
 <?php
 // Routes
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+$app->get('/branches', function(Request $request, Response $response, $args) {
+    $this->logger->info("Slim-Skeleton '/branches' route");
+
+    $data = array();
+    $response->withJson($data);
+    
+    return $response;
+});
+
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
