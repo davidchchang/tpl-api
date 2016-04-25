1b7dd7c023825b8be14c9505706b9d6adbd56ae0
David Chang <david.chang@chefsplate.com>
2016-04-25 00:36:21 -0400

    Add basic demo of API in action

--- src/routes.php
+++ src/routes.php
@@ -9,9 +9,18 @@ $app->get('/branches', function(Request $request, Response $response, $args) {
 
     $file_contents = file_get_contents('../public/branches.json');
     $data = json_decode($file_contents, true);
-    
+
     return $response->withJson($data);
 });
+
+$app->get('/demo', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/demo' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'demo.phtml', $args);
+});
+
 
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
